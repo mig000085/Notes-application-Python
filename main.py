d9ad4e2cfe885b372d1f0d361757c92ae988c555
Ivan Mokeev <mig00085@gmail.com>
2024-08-10 10:03:10 +0400

    change_note

--- main.py
+++ main.py
@@ -68,5 +68,25 @@ def id_notes_list():
             for row in data:
                 if row[3][:10] == time_note:
                     print(f'Номер записи: {row[0]}\nЗаголовок: {row[1]}\nЗаметка: {row[2]}\nДата: {row[3]}')
-
+def change_note():
+    list = []
+    change_id = int(input('Введите номер записи для изменения '))
+    if check_id(change_id):
+        with open ('notes.csv', 'r', encoding='utf-8') as d:
+            data = csv.reader(d, delimiter=';', skipinitialspace=True)
+            for rows in data:
+                if int(rows[0]) != change_id:                    
+                    list.append(';'.join(rows))
+                else:
+                    id_notes = rows[0]
+                    title = input('Измените загловок заметки ')
+                    note_body = input('Измените текст заметки ')
+                    date_time_create = datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M")
+                    list.append(f'{id_notes};{title};{note_body};{date_time_create}')
+        with open ('notes.csv', 'w+', encoding='utf-8') as f:
+            for item in list:
+                f.writelines(f'{item}\n')
+        print('Изменено')
+    else:
+        print('Такой записи нет')
         
